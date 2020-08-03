15bc8cb516eddf742c6dc583976efeb841560279
AnansiOmega <ignasbutautas7@gmail.com>
2020-08-03 18:39:01 -0500

    done

--- run.rb
+++ run.rb
@@ -7,31 +7,46 @@ spicy_foods = [
 ]
 
 def print_spicy_foods(spicy_foods)
-
+  message = ""
+spicy_foods.each do |key|
+ message = puts "#{key[:name]} (#{key[:cuisine]}) | Heat Level: #{key[:heat_level]} " 
+end
+message
 end
 
 def get_names(spicy_foods)
-
+list_of_food = spicy_foods.map do |key|
+ key[:name]
+  end
+  list_of_food
 end
 
 def spiciest_foods(spicy_foods)
-
+result = spicy_foods.select do |key|
+  key[:heat_level] > 5
+  end
+  result
 end
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-
+spicy_foods.find do |key|
+  if key[:cuisine] == cuisine
+    return key
+  end
+end
 end
 
 # BONUS Deliverables
 def print_spiciest_foods(spicy_foods)
-
+  print_spicy_foods(spiciest_foods(spicy_foods))
 end
 
 def average_heat_level(spicy_foods)
-
+heat = spicy_foods.map do |k|
+  k[:heat_level]
+end
+heat.sum(0.0) / heat.size
 end
 
 # Use this to test your methods
 # run "ruby run.rb" and try calling the methods from the console
-binding.pry
-"pls"
